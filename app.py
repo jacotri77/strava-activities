e6e4f402b79afc43aba45a5de95e99af298e9e1a
Erik Jacobsen <erikjacobsen@X-100022-LV.local>
2021-03-19 20:00:07 -0700

    adding some routing

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
+import secrets
 
-
+secret_key = secrets.token_hex(32)
+#print(secret_key)
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -17,6 +19,7 @@ def get_post(post_id):
     return post
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
 
 @app.route('/')
 def index():
@@ -29,4 +32,43 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
-    
+
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE posts SET title = ?, content = ?'
+                         ' WHERE id = ?',
+                         (title, content, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)    
